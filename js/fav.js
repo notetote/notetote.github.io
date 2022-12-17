1f3523084643cf3e5451a78d892262f9b153d4a9
NaeemBolchhi <42773187+NaeemBolchhi@users.noreply.github.com>
2022-12-17 23:36:17 +0600

    Add files via upload

--- js/fav.js
+++ js/fav.js
@@ -3,11 +3,11 @@ const colorScheme = window.matchMedia("(prefers-color-scheme: dark)"),
 
 function favSwitch() {
     if (colorScheme.matches) {
-        shortcutIcon[0].setAttribute("href","app/32w.png");
-        shortcutIcon[1].setAttribute("href","app/16w.png");
+        shortcutIcon[0].setAttribute("href","app/favW.ico");
+        //shortcutIcon[1].setAttribute("href","app/16w.png");
     } else {
-        shortcutIcon[0].setAttribute("href","app/32b.png");
-        shortcutIcon[1].setAttribute("href","app/16b.png");
+        shortcutIcon[0].setAttribute("href","app/favB.ico");
+        //shortcutIcon[1].setAttribute("href","app/16b.png");
     }
 }
 
