84bf2911240a108d408b0b85fa1fe45af054e242
NaeemBolchhi <42773187+NaeemBolchhi@users.noreply.github.com>
2022-12-18 23:48:14 +0600

    Add files via upload

--- js/fav.js
+++ js/fav.js
@@ -1,13 +1,31 @@
 const colorScheme = window.matchMedia("(prefers-color-scheme: dark)"),
-      favIcon = document.querySelectorAll("head link[rel='icon']");
+      linkRel = document.querySelectorAll("head link[rel]");
 
 function favSwitch() {
     if (colorScheme.matches) {
-        favIcon[0].setAttribute("href","app/favW.ico");
-        //shortcutIcon[1].setAttribute("href","app/16w.png");
+        for (let x = 0; x < linkRel.length; x++) {
+            linkRel[x].href = linkRel[x].href.replace("w.png",".png")
+                                             .replace("b.png",".png")
+                                             .replace(".png","w.png")
+                                             .replace("w.webmanifest",".webmanifest")
+                                             .replace("b.webmanifest",".webmanifest")
+                                             .replace(".webmanifest","w.webmanifest")
+                                             .replace("w.ico",".ico")
+                                             .replace("b.ico",".ico")
+                                             .replace(".ico","w.ico");
+        }
     } else {
-        favIcon[0].setAttribute("href","app/favB.ico");
-        //shortcutIcon[1].setAttribute("href","app/16b.png");
+        for (let x = 0; x < linkRel.length; x++) {
+            linkRel[x].href = linkRel[x].href.replace("w.png",".png")
+                                             .replace("b.png",".png")
+                                             .replace(".png","b.png")
+                                             .replace("w.webmanifest",".webmanifest")
+                                             .replace("b.webmanifest",".webmanifest")
+                                             .replace(".webmanifest","b.webmanifest")
+                                             .replace("w.ico",".ico")
+                                             .replace("b.ico",".ico")
+                                             .replace(".ico","b.ico");
+        }
     }
 }
 
