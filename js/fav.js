efb21842133f80a516a2bb6442f709ecbd3eedbb
NaeemBolchhi <42773187+NaeemBolchhi@users.noreply.github.com>
2022-12-19 00:12:20 +0600

    Add files via upload

--- js/fav.js
+++ js/fav.js
@@ -1,30 +1,17 @@
 const colorScheme = window.matchMedia("(prefers-color-scheme: dark)"),
-      linkRel = document.querySelectorAll("head link[rel]");
+      linkRel = document.querySelectorAll("head link[rel]:not([rel*='apple'],[rel*='canonical'],[rel*='mask'])");
 
 function favSwitch() {
+    for (let x = 0; x < linkRel.length; x++) {
+        linkRel[x].href = linkRel[x].href.replace("w.",".").replace("b.",".");
+    }
     if (colorScheme.matches) {
         for (let x = 0; x < linkRel.length; x++) {
-            linkRel[x].href = linkRel[x].href.replace("w.png",".png")
-                                             .replace("b.png",".png")
-                                             .replace(".png","w.png")
-                                             .replace("w.webmanifest",".webmanifest")
-                                             .replace("b.webmanifest",".webmanifest")
-                                             .replace(".webmanifest","w.webmanifest")
-                                             .replace("w.ico",".ico")
-                                             .replace("b.ico",".ico")
-                                             .replace(".ico","w.ico");
+            linkRel[x].href = linkRel[x].href.replace(".","w.");
         }
     } else {
         for (let x = 0; x < linkRel.length; x++) {
-            linkRel[x].href = linkRel[x].href.replace("w.png",".png")
-                                             .replace("b.png",".png")
-                                             .replace(".png","b.png")
-                                             .replace("w.webmanifest",".webmanifest")
-                                             .replace("b.webmanifest",".webmanifest")
-                                             .replace(".webmanifest","b.webmanifest")
-                                             .replace("w.ico",".ico")
-                                             .replace("b.ico",".ico")
-                                             .replace(".ico","b.ico");
+            linkRel[x].href = linkRel[x].href.replace(".","b.");
         }
     }
 }
