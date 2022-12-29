e4a7c89a625441c9cb3abc3066a95b292fb7ebc7
NaeemBolchhi <42773187+NaeemBolchhi@users.noreply.github.com>
2022-12-29 22:51:10 +0600

    Add files via upload

--- appendix/orator.js
+++ appendix/orator.js
@@ -3,7 +3,6 @@ const version = 1;
 
 // Path
 const path = window.location.pathname.replace(/\//g,"");
-let pathy;
 
 // Function for adding script to head or body
 function addScript(key, where) {
@@ -46,26 +45,20 @@ async function fetchThis(link) {
 // Function for path redirection
 function pathway() {
     if (!localStorage.version || localStorage.version < version || path === "load") {
-        pathy = "path_load.min.js";
-        return pathy;
+        return "path_load.min.js";
     }
 
     try {
         fetchThis("/appendix/path_" + path + ".min.js");
     } catch(e) {
-        pathy = "path_404.min.js";
-        return pathy;
+        return "path_404.min.js";
     }
 
     if (path === "") {
-        pathy = "path_home.min.js";
-        //return "path_home.min.js";
+        return "path_home.min.js";
     } else {
-        pathy = "path_" + path + ".min.js";
-        //return "path_" + path + ".min.js";
+        return "path_" + path + ".min.js";
     }
-
-    return "reg_base.min.js";
 }
 
 // Remove all appendix js from DOM
@@ -83,7 +76,7 @@ function appendectomy() {
         addScript("lib/pako_deflate.min.js", true);
         addScript("lib/localforage.min.js", true);
     }
-    if (pathy.match(/_search\./)) {
+    if (pathway().match(/_search\./)) {
         addScript("lib/localforage.min.js", true);
         addScript("lib/pako_inflate.min.js", true);
     }
--- appendix/path_home.js
+++ appendix/path_home.js
@@ -1,26 +1,36 @@
-document.querySelector("content").className = "flex flex-column";
-document.querySelector("main").className = "flex flex-column flex-center-h flex-center-v flex-grow container";
-
-document.querySelector("main").innerHTML = `
-<div class="logo-main margin m2 bottom">
-${localGet("logotextbelow")}
-</div>
-<form class="flex flex-row search-box margin m1 bottom" action="search.html">
-<input name="s" class="flex-grow" value="" type="text" autocomplete="off" required>
-<input class="fa small hide" value="X" type="button" onclick="clearSearch()">
-<input class="fa" value="S" type="submit">
-</form>
-<div class="margin m2 bottom">
-<span class="font-light">What are we studying today?</span>
-</div>
-<div class="flex flex-row flex-wrap flex-center-h search-suggest font-light margin m2 bottom">
-<a onclick="quickSearch(this)">Macbeth</a>
-<a onclick="quickSearch(this)">Shakespeare</a>
-<a onclick="quickSearch(this)">Romeo and Juliet</a>
-<a onclick="quickSearch(this)">Hammer</a>
-<a onclick="quickSearch(this)">Macbeth</a>
-<a onclick="quickSearch(this)">Shakespeare</a>
-<a onclick="quickSearch(this)">Romeo</a>
-<a onclick="quickSearch(this)">Hamlet</a>
-</div>
-`;
+document.getElementsByTagName("body")[0].innerHTML = `
+<content class="flex flex-column">
+  <header class="flex flex-column flex-center-h container">
+    <span class="flex flex-row flex-reverse flex-center-v fix-height pointer">
+      <i class="fa fa-menu"></i>
+    </span>
+  </header>
+  <main class="flex flex-column flex-center-h flex-center-v flex-grow container">
+    <div class="logo-main margin m2 bottom">${localGet("logotextbelow")}</div>
+    <form class="flex flex-row search-box margin m1 bottom" action="search.html">
+      <input name="s" class="flex-grow" value="" type="text" autocomplete="off" required>
+      <input class="fa small hide" value="X" type="button" onclick="clearSearch()">
+      <input class="fa" value="S" type="submit">
+    </form>
+    <div class="margin m2 bottom">
+      <span class="font-light">What are we studying today?</span>
+    </div>
+    <div class="flex flex-row flex-wrap flex-center-h search-suggest font-light margin m2 bottom">
+      <a onclick="quickSearch(this)">Macbeth</a>
+      <a onclick="quickSearch(this)">Shakespeare</a>
+      <a onclick="quickSearch(this)">Romeo and Juliet</a>
+      <a onclick="quickSearch(this)">Hammer</a>
+      <a onclick="quickSearch(this)">Macbeth</a>
+      <a onclick="quickSearch(this)">Shakespeare</a>
+      <a onclick="quickSearch(this)">Romeo</a>
+      <a onclick="quickSearch(this)">Hamlet</a>
+    </div>
+  </main>
+  <footer class="flex flex-row flex-space-between container font-light">
+    <span>
+      <a href="https://notenibba.github.io/">
+        <i class="fa fa-notenibba margin m0 right"></i>NoteNibba </a> | <a href="https://www.gnu.org/licenses/gpl-3.0-standalone.html">GPL-3.0</a>
+    </span>
+    <span>Loaded in X secs.</span>
+  </footer>
+</content>`;
