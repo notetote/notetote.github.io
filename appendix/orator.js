04039940dec955298438d22bf475077bb2a8022e
NaeemBolchhi <42773187+NaeemBolchhi@users.noreply.github.com>
2023-01-23 23:57:40 +0600

    Add files via upload

--- appendix/orator.js
+++ appendix/orator.js
@@ -1,5 +1,5 @@
 // Version Extension
-const version = 20230023235134,
+const version = 20230023235711,
       datever = "?" + version.toString();
 
 // Declare app state if using app
--- appendix/path_search.js
+++ appendix/path_search.js
@@ -2,17 +2,17 @@ document.title = "Search at NoteNibba";
 
 document.getElementsByTagName("body")[0].innerHTML = localGet("fable_search");
 
-document.getElementsByTagName("scrolltop")[0].outerHTML = localGet("fable_scroll");
+document.getElementsByTagName("scrolltop")[0].outerHTML = localGet("parable_scroll");
 
-document.getElementsByTagName("header")[0].outerHTML = localGet("fable_header");
+document.getElementsByTagName("header")[0].outerHTML = localGet("parable_header");
 
 document.getElementsByClassName("logo-top")[0].innerHTML = localGet("svg_side") + localGet("svg_none");
 
 document.getElementsByClassName("logo-side")[0].innerHTML = localGet("svg_side") + localGet("svg_none");
 
-document.getElementsByTagName("footer")[0].outerHTML = localGet("fable_footer");
+document.getElementsByTagName("footer")[0].outerHTML = localGet("parable_footer");
 
-document.getElementsByTagName("nav")[0].outerHTML = localGet("fable_nav");
+document.getElementsByTagName("nav")[0].outerHTML = localGet("parable_nav");
 
 localforage.getItem("index").then(function(event) {
     let txt = new TextDecoder().decode(pako.inflate(event));
